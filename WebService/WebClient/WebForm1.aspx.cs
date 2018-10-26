bbc1f3c92bc2be6d341ebb2c3e980d4bd68da3fa
Kovalev Aleksey <akovalev@ics.perm.ru>
2018-10-27 01:35:46 +0500

    Add

---
+++ WebService/WebClient/WebForm1.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebClient.CalculatorService;//пространтсво имён веб-сервиса
+
+namespace WebClient
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private static WebServiceCalculatorSoapClient _serviceCalculatorSoapClient;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Добавлено веб-ссылка на сервис
+            // Это инициализация клиента для веб-сервиса
+            _serviceCalculatorSoapClient = new CalculatorService.WebServiceCalculatorSoapClient();
+        }
+
+        
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            if (_serviceCalculatorSoapClient.State != CommunicationState.Opened)
+            {
+                var result = "";
+                if (button == Button1)
+                {
+                    result =
+                        _serviceCalculatorSoapClient.Sub(double.Parse(TextBox1.Text), double.Parse(TextBox2.Text))
+                            .ToString(CultureInfo.CurrentCulture);
+                }
+                else if (button == Button2)
+                {
+                    result =
+                        _serviceCalculatorSoapClient.Sum(double.Parse(TextBox1.Text), double.Parse(TextBox2.Text))
+                            .ToString(CultureInfo.CurrentCulture);
+                }
+                else if (button == Button3)
+                {
+                    result =
+                        _serviceCalculatorSoapClient.Div(double.Parse(TextBox1.Text), double.Parse(TextBox2.Text))
+                            .ToString(CultureInfo.CurrentCulture);
+                }
+                else
+                {
+                    result =
+                        _serviceCalculatorSoapClient.Mult(double.Parse(TextBox1.Text), double.Parse(TextBox2.Text))
+                            .ToString(CultureInfo.CurrentCulture);
+                }
+
+                Label1.Text = result;
+                _serviceCalculatorSoapClient.Close();
+            }
+        }
+    }
+}
---
+++ WebService/WebService/WebServiceCalculator.asmx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebService
+{
+    /// <summary>
+    /// Summary description for WebServiceCalculator
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebServiceCalculator : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public double Sum(double x, double y)
+        {
+            return x+y;
+        }
+
+        [WebMethod]
+        public double Sub(double x, double y)
+        {
+            return x - y;
+        }
+
+        [WebMethod]
+        public double Div(double x, double y)
+        {
+            return x / y;
+        }
+
+        [WebMethod]
+        public double Mult(double x, double y)
+        {
+            return x * y;
+        }
+    }
+}
